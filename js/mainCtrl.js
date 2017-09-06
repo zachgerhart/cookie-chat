2d97370347c61e9c2dc77deeb392e9eb68a0e8de
zachgerhart <zgerhart@gmail.com>
2017-09-06 16:15:36 -0500

    modified

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,22 +1,26 @@
 angular.module('chatroom').controller('mainCtrl', function($scope, messageService){
-
   //The getMessages function will call the getMessages method on the messageService object.
   //You'll then save the result of that request to your controller's $scope as messages ($scope.messages)
 
-
-
+$scope.getMessages = function() {
+  return messageService.getMessages().then(function(response){
+    $scope.messages = response.data.reverse()
+  })
+}
 
   //The postMessage function will take whatever the user typed in (hint: look at the html and see what ng-model correlates to on the input box),
   //pass that text to the postMessage method on the messageService object which will
   //then post it to the backend.
 
-
+    $scope.postMessage = function(message){
+      return messageService.postMessage(message)
+    }
 
   //uncomment this code when your getMessages function is finished
   //This goes and gets new data every second, which mimicking a chat room experience.
-  //  setInterval(function(){
-    // $scope.getMessages();
+   setInterval(function(){
+    $scope.getMessages();
 
-  // }, 1500)
+  }, 1500)
 
 })
--- js/messageService.js
+++ js/messageService.js
@@ -5,15 +5,25 @@ angular.module('chatroom').service('messageService', function($http){
   //The url for the get request should be 'https://practiceapi.devmountain.com/api/chats'
   //Be sure to return whatever gets returned from $http so you can call .then in your controller.
 
-
-
+var apiurl = "https://practiceapi.devmountain.com/api/chats"
+  this.getMessages = function(){
+    return $http.get(apiurl)
+  }
 
   //On the line below create the postMessage method. This method will add data to the backend server.
   //The url for the request needs to be 'https://practiceapi.devmountain.com/api/chats'
   //Because we're making a POST request, need a data object with a message property IE data: {message: "Hello World"}
   //Also, remember that $http returns a promise. So return the whole $http call (return $http(...)), so you can then use .then in your controller.
+  this.postMessage = function(message){
+    return $http({
+      method: "post",
+      url: apiurl,
+      data: {
+        message: message
+      }
+    })
 
-
+  }
 
 
 });
